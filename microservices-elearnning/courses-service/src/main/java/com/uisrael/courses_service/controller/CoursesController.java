a72576b2da6360568881209727ca5840779a3960
EddWlad <tidsec_inf@outlook.com>
2024-09-08 20:08:06 -0500

    Confirguraciones varias

--- microservices-elearnning/courses-service/src/main/java/com/uisrael/courses_service/controller/CoursesController.java
+++ microservices-elearnning/courses-service/src/main/java/com/uisrael/courses_service/controller/CoursesController.java
@@ -3,7 +3,7 @@ package com.uisrael.courses_service.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
